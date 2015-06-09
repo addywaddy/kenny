b847788ddbdc9d1f9812b0faf7c47936cbd65aa0
Adam Groves <adam.groves@gmail.com>
2015-06-09 09:08:02 +0200

    Starting to make gravity a central function of the game
    The vertical-displacement of the hero is constantly being calculated,
    based on the blocks below both it's feet.
    
    Next:
    1. Restrict horizontal movement if block in way
    2. Reimplement jumping

--- src/cljs/kenny/core.cljs
+++ src/cljs/kenny/core.cljs
@@ -8,51 +8,70 @@
 
 (def app-state (atom {:text "Hello Chestnut!"
                       :hero {:move false
-                             :position {:bottom 211 :left 209}
+                             :position {:bottom 560 :left 0}
                              :jump false
                              }
 
                       :grid [[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
                              [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                             [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                             [1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0]
                              [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
                              [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
                              [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
                              [0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                             [0 0 0 0 5 5 4 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                             [7 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 8]
+                             [0 0 0 0 5 5 4 4 0 0 0 0 0 0 0 0 0 0 0 0]
+                             [1 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 8]
                              [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
                              ]}))
+
+(defn ceil [i]
+  (.ceil js/Math i))
+
+(defn floor [i]
+  (.floor js/Math i))
 
 (defn hero-position [app]
   #js {:bottom (get-in app [:hero :position :bottom])
        :left (get-in app [:hero :position :left])})
 
+(defn hero-feet-coords [app]
+  (let [bottom (get-in app [:hero :position :bottom])
+        left (get-in app [:hero :position :left])
+        hero-feet-offset 20]
+    [
+     [(- 9 (floor (/ bottom 70))) (floor (/ (+ left 20) 70))]
+     [(- 9 (floor (/ bottom 70))) (floor (/ (+ left 50) 70))]
+     ]
+    ))
+
+(defn most-supportive-block [app]
+  (let [[left right] (hero-feet-coords app)]
+    (max (get-in app (into [:grid] left))
+         (get-in app (into [:grid] right))
+         )))
+
 (def g 2000)
 (def v 1000)
 
-(defn transpose [m]
-  (apply mapv vector m))
-
-(defn floor [i]
-  (.floor js/Math i))
-
-(defn ceil [i]
-  (.ceil js/Math i))
-
-(defn floor-coord [px]
-  (floor (/ px 70)))
-
-(defn ceil-coord [px]
-  (ceil (/ px 70)))
-
-(defn hero-coords [app]
-  (let [left-pos (get-in @app [:hero :position :left])
-        bottom-pos (get-in @app [:hero :position :bottom])]
-    [(ceil-coord left-pos), (floor-coord bottom-pos)]))
+(defn is-solid? [app [grid-x grid-y]]
+  (>= (get-in app [:grid (- 9 grid-y) grid-x]) 1)
+  )
 
 (defn current-standing-height []
   70)
+
+(def gravitational-force 10)
+
+(defn on-solid-ground? [app]
+  true)
+
+(defn vertical-position [app]
+  (let [current-vertical (get-in app [:hero :position :bottom])
+        current-block-no (most-supportive-block app)]
+    (if (>= current-block-no 1)
+      current-vertical
+      (- current-vertical 1)
+  )))
 
 (defn gravity [app]
   (let [dy (get-in @app [:hero :position :bottom])
@@ -63,11 +82,32 @@
         gv (/ (* g dt) 2)
         height (+ (- vforce gforce) (current-standing-height))
         ]
-    (if (> 70 height)
+    (if (or
+         (is-solid? @app (first (hero-feet-coords @app)))
+         (is-solid? @app (last (hero-feet-coords @app)))
+         )
+      (* 70  (last (first (hero-feet-coords @app))))
       70
       height
       ))
   )
+
+
+(defn step-class [app]
+  (let [left-offset (get-in app [:hero :position :left])]
+    (rem (ceil (/ left-offset 15)) 3)
+    ))
+
+(defn hero-classes [app]
+  (let [
+        left-class (when (= :left (get-in app [:hero :move]))
+          "left "
+          )]
+    (if (get-in app [:hero :move])
+      (str "hero " left-class "step-" (step-class app))
+      (str "hero " left-class)
+      )
+    ))
 
 (defn hero [app owner]
   (reify
@@ -75,20 +115,15 @@
     (will-mount [_]
       (js/setInterval
        (fn []
-         (when (get-in @app [:hero :jump])
-           (let [height (gravity app)]
-             (when (= height 70)
-               (om/update! app [:hero :jump] false))
-             (om/update! app [:hero :position :bottom] height)
-             ))
+         (om/update! app [:hero :position :bottom] (vertical-position @app))
          (when (= :left (get-in @app [:hero :move]))
-           (om/transact! app [:hero :position :left] (fn [old] (+ old 5))))
-         (when (= :right (get-in @app [:hero :move]))
            (om/transact! app [:hero :position :left] (fn [old] (- old 5))))
+         (when (= :right (get-in @app [:hero :move]))
+           (om/transact! app [:hero :position :left] (fn [old] (+ old 5))))
          ) (/ 1000 60)))
     om/IRenderState
     (render-state [this state]
-      (dom/div #js {:className "hero" :style (hero-position app)} nil))
+      (dom/div #js {:className (hero-classes app) :style (hero-position app)} nil))
       ))
 
 (def id->tile-class
@@ -126,8 +161,8 @@
 
 (defn start-moving [e app]
   (condp = (aget e "keyCode")
-    88 (om/update! app [:hero :move] :left)
-    90 (om/update! app [:hero :move] :right)
+    88 (om/update! app [:hero :move] :right)
+    90 (om/update! app [:hero :move] :left)
     32 (when-not (get-in @app [:hero :jump])
          (om/update! app [:hero :jump] (.getTime (js/Date.))))
     nil
